49abded46d6efbb62a9d5e398dcccbf44b8a4112
Igengefashionshop <faboykv70@gmail.com>
2026-09-24 23:19:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const WHATSAPP_NUMBER = "25766212337";
 const products = [
   {id:1,name:"Elegant Dress",price:45000,category:"women",tag:"NEW",image:""},
   {id:2,name:"Classic Top",price:10000,category:"women",tag:"",image:""},
-  {id:3,name:"Wide Leg Trousers",price:60000,category:"women",tag:"TRENDING",image:""},
+  {id:3,name:"Screenshot_20260923-210443.jpg",price:60000,category:"women",tag:"TRENDING",image:""},
   {id:4,name:"Women's Dress",price:25000,category:"women",tag:"NEW",image:""},
   {id:5,name:"Men's Shirt",price:15000,category:"men",tag:"",image:""},
   {id:6,name:"Classic Jeans",price:35000,category:"men",tag:"",image:""},
